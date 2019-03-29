189d9588b114c1529b468189f47c4dd7264fe2d5
Bordak Tamas <tamas@webtar.hu>
2019-03-29 18:19:18 +0100

    pathfinding

--- index.js
+++ index.js
@@ -61,9 +61,23 @@ map = ["GPSSPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGPSSPG",
 "GPSSPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGPSSPG"];
 
 var net = require('net');
+var PF = require('pathfinding');
 
 var client = new net.Socket();
 token = "XCm41v5wXaM3TzNraLdRNIoHH159v1PXaQiE2ARfNDmF1RQuxmYzTCm3HJWGuGsuGckiDuw";
+
+var height = map.length;
+var width = map[0].length;
+console.log(height)
+console.log(width)
+
+var walkable = map.map(x => x.split('').map(x => x == 'S' ? 0 : 1))
+
+var grid = new PF.Grid(walkable);
+var finder = new PF.AStarFinder();
+var path = finder.findPath(2, 2, 8, 2, grid);
+console.log(path)
+
 
 client.connect(12323, '31.46.64.35', function() {
 	client.write(JSON.stringify({token}));
